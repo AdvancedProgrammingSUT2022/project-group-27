a8dcade8432024352c57957fde3262b58faa9860
Arefe Boushehrian <arefeboushehrian@Arefes-MacBook-Pro.local>
2022-05-08 08:43:18 -0700

    trying to write a test for navigation for main menu

--- src/test/java/MainMenuTest.java
+++ src/test/java/MainMenuTest.java
@@ -1,16 +1,23 @@
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
 import Enum.*;
+import view.GameMenu;
 import view.MainMenu;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -19,10 +26,15 @@ import java.lang.reflect.Method;
 public class MainMenuTest {
 
     @Test
-    public void navigationTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        final var mainMenuClass = MainMenu.getInstance();
-        Method method = mainMenuClass.getClass().getDeclaredMethod("enterMenu", String.class);
+    public void navigationTestForFalse() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        MainMenu menu = spy(MainMenu.class);
+        doNothing().when(menu).run();
+
+        Method method = MainMenu.class.getDeclaredMethod("enterMenu", String.class);
         method.setAccessible(true);
-        method.invoke(mainMenuClass);
+
+        method.invoke(menu, "some random strings");
+        method.invoke(menu, "Main_Menu");
+        method.invoke(menu, "Login_Menu");
     }
 }
