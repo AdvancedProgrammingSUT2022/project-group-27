58038700877bf1d81b1d68e8893b55bc60f8577e
aalavirr <aryan.alaviarar@gmail.com>
2022-05-13 01:34:44 +0430

    melee to city added

--- src/main/java/model/MeleeUnit.java
+++ src/main/java/model/MeleeUnit.java
@@ -1,6 +1,7 @@
 package model;
 
 import Enum.MilitaryType;
+import view.game.ConquerCityMenu;
 
 public class MeleeUnit extends MilitaryUnit {
     public MeleeUnit(Ground ground, Player player, MilitaryType militaryType) {
@@ -48,6 +49,32 @@ public class MeleeUnit extends MilitaryUnit {
     }
     @Override
     public void combat(City city) {
+        MilitaryUnit militaryUnit = city.getGround().getMilitaryUnit();
+        UnMilitaryUnit unMilitaryUnit = city.getGround().getUnMilitaryUnit();
+        double decreasedEnemyHp = (this.hp + 10) / 20 * this.militaryType.getCombatStrength();
+        decreasedEnemyHp *= (double) 100.0 / (ground.getGroundType().getCombatCoefficient() + 100.0);
+        double decreasedOwnHp = city.getCityStrength();
+        if (militaryUnit != null)
+            decreasedOwnHp += (militaryUnit.hp + 10) / 20 * this.militaryType.getCombatStrength() / 3;
+        this.hp -= decreasedOwnHp;
+        city.setHp(city.getHp() - decreasedEnemyHp);
+        boolean isThisDestroyed = false;
+        if (this.hp <= 0.0001) {
+            this.removeUnit();
+            isThisDestroyed = true;
+        }
+        if (city.getHp() <= 0.0001) {
+            if (militaryUnit != null)
+                militaryUnit.removeUnit();
+            if (unMilitaryUnit != null)
+                unMilitaryUnit.removeUnit();
+            if (!isThisDestroyed)
+                this.ground = city.getGround();
+            ConquerCityMenu.run(city);
+
+        }
+        if (this.militaryType.equals(MilitaryType.HORSEMAN) || this.militaryType.equals(MilitaryType.KNIGHT) || this.militaryType.equals(MilitaryType.CAVALRY) || this.militaryType.equals(MilitaryType.LANCER) || this.militaryType.equals(MilitaryType.PANZER) || this.militaryType.equals(MilitaryType.TANK))
+            mp = 10;
 
     }
 }
---
+++ src/main/java/view/game/ConquerCityMenu.java
@@ -0,0 +1,9 @@
+package view.game;
+
+import model.City;
+
+public class ConquerCityMenu {
+    public static void run(City city) {
+
+    }
+}
