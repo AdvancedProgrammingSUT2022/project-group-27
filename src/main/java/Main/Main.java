8b25377efd6a6d1e1616ddb0e69098efb19d8a38
Arefe Boushehrian <arefeboushehrian@Arefes-MacBook-Pro.local>
2022-05-27 01:28:47 -0700

    fixing place of database

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -12,21 +12,41 @@ import java.io.IOException;
 import java.net.URL;
 
 public class Main extends Application {
+    private static Stage stage;
+
+    public static Stage getStage() {
+        return stage;
+    }
+
     public static void main(String[] args) throws IOException {
         launch();
-        Database.readFromDatabase();
 
-        LoginMenu.getInstance().run();
+        LoginMenu.getInstance().run(); //TODO change these things to graphic
 
-        Database.writeOnDataBase();
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Database.writeOnDataBase();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }));
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        URL address = new URL(Main.class.getResource("/welcome.fxml").toExternalForm());
-        Parent root = FXMLLoader.load(address);
+        Database.readFromDatabase();
+        Main.stage = stage;
+        Parent root = loadFXML("welcome");
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static Parent loadFXML(String name) throws IOException {
+        URL address = new URL(Main.class.getResource("/" + name + ".fxml").toExternalForm());
+        return FXMLLoader.load(address);
     }
 }
