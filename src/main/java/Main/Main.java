107873d7656a75e09f5a4c32e106f47224dc5abc
Arefe Boushehrian <arefeboushehrian@Arefes-MacBook-Pro.local>
2022-05-27 08:36:22 -0700

    adding audio and background to welcome page

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,10 +2,14 @@ package Main;
 
 import database.Database;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import view.LoginMenu;
 
 import java.io.IOException;
@@ -13,6 +17,7 @@ import java.net.URL;
 
 public class Main extends Application {
     private static Stage stage;
+    private static MediaPlayer audio;
 
     public static Stage getStage() {
         return stage;
@@ -35,14 +40,35 @@ public class Main extends Application {
         }));
     }
 
+    @FXML
+    public void initialize() {
+        audio.play();
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
         Database.readFromDatabase();
+        audio = loadingAudio("foxTwentyCentry.mp4");
         Main.stage = stage;
         Parent root = loadFXML("welcome");
+        root.getStylesheets().add(Main.class.getResource("/css/welcome.css").toExternalForm());
+        root.getStyleClass().add("background");
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static MediaPlayer loadingAudio(String name) {
+        Media audio = new Media(Main.class.getResource("/audio/" + name).toExternalForm());
+        MediaPlayer mediaPlayer = new MediaPlayer(audio);
+        mediaPlayer.setAutoPlay(true);
+        mediaPlayer.setOnEndOfMedia(new Runnable() {
+            @Override
+            public void run() {
+                mediaPlayer.seek(Duration.ZERO);
+            }
+        });
+        return mediaPlayer;
     }
 
     public static Parent loadFXML(String name) throws IOException {
