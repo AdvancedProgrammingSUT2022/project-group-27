e7cc49876bb494ecb68e663a8f0b7af05d6517eb
Arefe Boushehrian <arefeboushehrian@Arefes-MacBook-Pro.local>
2022-05-29 03:20:36 -0700

    complete logic and view of profile menu except changing image

---
+++ src/main/java/Enum/MenusInProfile.java
@@ -0,0 +1,7 @@
+package Enum;
+
+public enum MenusInProfile {
+    CHANGING_PASSWORD,
+    CHANGING_NICKNAME,
+    CHANGING_PROFILE_IMAGE;
+}
--- src/main/java/viewControllers/ProfileView.java
+++ src/main/java/viewControllers/ProfileView.java
@@ -4,10 +4,13 @@ import Main.Main;
 import controller.ProfileController;
 import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -16,11 +19,36 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 import model.User;
 import view.Menu;
+import Enum.MenusInProfile;
+import Enum.Message;
 
 public class ProfileView extends Application {
     private static Stage stage;
     private static MediaPlayer audio;
     private static final User user = Menu.getLoggedInUser();
+    private static MenusInProfile menus;
+
+    private TextField newNickname;
+    private TextField newPassword;
+    private TextField oldPassword;
+
+    @FXML
+    private Button password;
+
+    @FXML
+    private Button nickname;
+
+    @FXML
+    private Button profile;
+
+    @FXML
+    private Button submit;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private VBox box;
 
     @FXML
     private Label username;
@@ -46,7 +74,10 @@ public class ProfileView extends Application {
         shadow.setColor(Color.WHITE);
         username.setEffect(shadow);
         username.setFont(new Font(14));
-        //TODO complete it...
+        back.setCursor(Cursor.HAND);
+        submit.setCursor(Cursor.HAND);
+
+        changePasswordButton();
     }
 
     @Override
@@ -60,5 +91,105 @@ public class ProfileView extends Application {
         stage.setScene(scene);
         stage.setTitle("Profile Menu");
         stage.show();
+    }
+
+    public void back(MouseEvent mouseEvent) throws Exception {
+        audio.stop();
+        MainMenuView mainMenuView = new MainMenuView();
+        mainMenuView.start(stage);
+    }
+
+    private void removingElementsFromBox() {
+        box.getChildren().remove(newNickname);
+        box.getChildren().remove(oldPassword);
+        box.getChildren().remove(newPassword);
+    }
+
+    public void changePasswordButton() {
+        menus = MenusInProfile.CHANGING_PASSWORD;
+        password.setDisable(true);
+        nickname.setDisable(false);
+        profile.setDisable(false);
+        removingElementsFromBox();
+        oldPassword = new TextField();
+        oldPassword.setPromptText("enter your old password");
+        newPassword = new TextField();
+        newPassword.setPromptText("enter your new password");
+        box.getChildren().add(0,newPassword);
+        box.getChildren().add(0, oldPassword);
+        box.setLayoutX(320);
+    }
+
+    public void changeNicknameButton(MouseEvent mouseEvent) {
+        menus = MenusInProfile.CHANGING_NICKNAME;
+        password.setDisable(false);
+        nickname.setDisable(true);
+        profile.setDisable(false);
+        removingElementsFromBox();
+        newNickname = new TextField();
+        newNickname.setPromptText("enter your new nickname");
+        box.getChildren().add(0, newNickname);
+        box.setLayoutX(320);
+    }
+
+    public void submitChanges(MouseEvent mouseEvent) {
+        switch (menus) {
+            case CHANGING_PASSWORD -> changePasswordAction();
+            case CHANGING_NICKNAME -> changeNicknameAction();
+        }
+    }
+
+    private void changePasswordAction() {
+        if (oldPassword.getLength() == 0 || newPassword.getLength() == 0) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("empty field");
+            alert.setContentText("sorry, but one of your necessary fields is empty");
+            alert.show();
+            return;
+        }
+
+        String oldPasswordString = oldPassword.getText();
+        String newPasswordString = newPassword.getText();
+
+        Message message = ProfileController.getInstance().changePassword(newPasswordString, oldPasswordString);
+        if (message != Message.PASSWORD_CHANGED) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("invalid field");
+            alert.setContentText(message.toString());
+            alert.show();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("Success change");
+            alert.setContentText(message.toString());
+            alert.show();
+            oldPassword.clear();
+            newPassword.clear();
+        }
+    }
+
+    private void changeNicknameAction() {
+        if (newNickname.getLength() == 0) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("empty field");
+            alert.setContentText("sorry, but one of your necessary fields is empty");
+            alert.show();
+            return;
+        }
+
+        String newNicknameString = newNickname.getText();
+
+        Message message = ProfileController.getInstance().changeNickname(newNicknameString);
+        if (message != Message.NICKNAME_CHANGED) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("invalid field");
+            alert.setContentText(message.toString());
+            alert.show();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("Success change");
+            alert.setContentText(message.toString());
+            alert.show();
+            newNickname.clear();
+        }
     }
 }
