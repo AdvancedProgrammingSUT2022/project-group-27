147b5678f30a21bfcfbe317ee45b078066f08185
mehrdadsohrabi <mehrdad.sohrabi.skm@gmail.com>
2022-06-03 22:09:20 +0430

    game menu

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -155,4 +155,22 @@ public class User {
         Comparator<User> full = sortByScore.thenComparing(sortByTime).thenComparing(sortByName);
         User.listOfUsers.sort(full);
     }
+    public static boolean doWeHaveAllOfTheseUsers(ArrayList<String> list){
+        for (String username : list){
+            boolean isOk=false;
+            for (User user : listOfUsers){
+                if (user.getUsername().equals(username)) isOk=true;
+            }
+            if (!isOk) return false;
+        }
+        return true;
+    }
+    public static boolean isThisListOfUsersUnique(ArrayList<String> list){
+        for (int i=0;i<list.size();i++){
+            for (int j=i+1;j<list.size();j++){
+                if (list.get(i).equals(list.get(j))) return false;
+            }
+        }
+        return true;
+    }
 }
--- src/main/java/viewControllers/GameView.java
+++ src/main/java/viewControllers/GameView.java
@@ -1,11 +1,57 @@
 package viewControllers;
 
+import Main.Main;
 import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import model.User;
+
+import java.util.ArrayList;
 
 public class GameView extends Application {
+    private static Stage stage;
+    private static MediaPlayer audio;
+    public TextField arrayOfEnemies;
+
     @Override
     public void start(Stage stage) throws Exception {
+        GameView.stage = stage;
+        audio = Main.loadingAudio("openingTitles.mpeg");
+        Parent root = Main.loadFXML("GameView");
+        root.getStylesheets().add(Main.class.getResource("/css/login.css").toExternalForm());
+        root.getStyleClass().add("background");
         System.out.println("game");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Game Menu");
+        stage.show();
+    }
+
+    public void startGame(MouseEvent mouseEvent) {
+    }
+
+    public void textFieldEditor(KeyEvent keyEvent) {
+        String s[]=arrayOfEnemies.getText().split(",");
+        ArrayList<String> arrayList=new ArrayList<>();
+        for (int i=0;i<s.length;i++){
+            if (s[i].trim().equals("")) continue;
+            arrayList.add(s[i].trim());
+        }
+        System.out.println(arrayList + " " + arrayOfEnemies.getText());
+        for (User user: User.getListOfUsers()){
+            System.out.println("username= : " + user.getUsername());
+        }
+        if (!(User.doWeHaveAllOfTheseUsers(arrayList) && User.isThisListOfUsersUnique(arrayList))){
+            arrayOfEnemies.setStyle("-fx-text-fill: red;");
+            System.out.println("odijfie");
+        }
+        else{
+            arrayOfEnemies.setStyle("-fx-text-fill: green;");
+        }
     }
 }
