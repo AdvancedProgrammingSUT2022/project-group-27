eefe913409f74dd3d71340f725bff4f80a1223ef
Arefe Boushehrian <arefeboushehrian@Arefes-MacBook-Pro.local>
2022-06-03 23:29:48 -0700

    adding first preview of chat room

--- src/main/java/viewControllers/ChatRoomView.java
+++ src/main/java/viewControllers/ChatRoomView.java
@@ -1,11 +1,53 @@
 package viewControllers;
 
+import Main.Main;
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class ChatRoomView extends Application {
+    private static Stage stage;
+    private static MediaPlayer audio;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    public void initialize() {
+        audio.play();
+        audio.setOnEndOfMedia(new Runnable() {
+            @Override
+            public void run() {
+                audio.seek(Duration.ZERO);
+            }
+        });
+
+        back.setCursor(Cursor.HAND);
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
-        System.out.println("chat room");
+        ChatRoomView.stage = stage;
+        audio = Main.loadingAudio("menu.m4a");
+        Parent root = Main.loadFXML("chat-room");
+        root.getStylesheets().add(Main.class.getResource("/css/chatRoom.css").toExternalForm());
+        root.getStyleClass().add("background");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Chat Room");
+        stage.show();
+    }
+
+    public void back(MouseEvent mouseEvent) throws Exception {
+        audio.stop();
+        MainMenuView mainMenuView = new MainMenuView();
+        mainMenuView.start(stage);
     }
 }
