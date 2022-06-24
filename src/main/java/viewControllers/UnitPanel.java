0905b4b4572af2ea30a097a30e2664ce157fb8aa
Arefe Boushehrian <arefeboushehrian@Arefes-MacBook-Pro.local>
2022-06-24 03:52:04 -0700

    every thing is done for unit panel except ranged and melee fight

--- src/main/java/viewControllers/UnitPanel.java
+++ src/main/java/viewControllers/UnitPanel.java
@@ -1,21 +1,30 @@
 package viewControllers;
 
 import Main.Main;
+import controller.UnitController;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.Player;
 import model.Unit;
+import Enum.*;
+
+import java.util.Optional;
 
 public class UnitPanel extends Menus {
     private static Stage stage;
     private static Player player;
     private static GraphicOfGame game;
+
+    @FXML
+    public Button back;
 
     @FXML
     private VBox list;
@@ -27,6 +36,7 @@ public class UnitPanel extends Menus {
         list.getChildren().add(firstText);
 
         addToList();
+        back.setCursor(Cursor.HAND);
     }
 
     public static void setPlayer(Player player) {
@@ -49,6 +59,148 @@ public class UnitPanel extends Menus {
         stage.show();
     }
 
+    private void showAlert(Message message) {
+        if (message != Message.SUCCESS_WORK) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Error");
+            alert.setContentText(message.toString());
+            alert.show();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("Success");
+            alert.setContentText(message.toString());
+        }
+    }
+
+    private ContextMenu buildContextMenu(Unit unit) {
+        final ContextMenu contextMenu = new ContextMenu();
+        MenuItem sleep = new MenuItem("sleep");
+        sleep.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                unit.setStatus(UnitStatus.SLEEP);
+            }
+        });
+
+        MenuItem ready = new MenuItem("ready");
+        ready.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                unit.setStatus(UnitStatus.READY);
+            }
+        });
+
+        MenuItem improving = new MenuItem("improving");
+        improving.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                unit.setStatus(UnitStatus.IMPROVING);
+            }
+        });
+
+        MenuItem improvingForHealth = new MenuItem("improving for health");
+        improvingForHealth.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                unit.setStatus(UnitStatus.HEALTH_IMPROVING);
+            }
+        });
+
+        MenuItem established = new MenuItem("established");
+        established.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if (!unit.getStatus().equals(UnitStatus.AWAKE)) unit.setStatus(UnitStatus.AWAKE);
+                Message message = UnitController.established(unit);
+                showAlert(message);
+            }
+        });
+
+        MenuItem rangedFight = new MenuItem("ranged fight");
+        rangedFight.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                //TODO setting it
+            }
+        });
+
+        MenuItem meleeFight = new MenuItem("melee fight");
+        meleeFight.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                //TODO setting it
+            }
+        });
+
+        MenuItem readyRangedFight = new MenuItem("ready to ranged fight");
+        readyRangedFight.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if (!unit.getStatus().equals(UnitStatus.AWAKE)) unit.setStatus(UnitStatus.AWAKE);
+                Message message = UnitController.readyToRangedFight(unit);
+                showAlert(message);
+            }
+        });
+
+        MenuItem plundering = new MenuItem("plundering");
+        plundering.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if (!unit.getStatus().equals(UnitStatus.AWAKE)) unit.setStatus(UnitStatus.AWAKE);
+                Message message = UnitController.plundering(unit);
+                showAlert(message);
+            }
+        });
+
+        MenuItem deleteOrder = new MenuItem("delete one order");
+        deleteOrder.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if (!unit.getStatus().equals(UnitStatus.AWAKE)) unit.setStatus(UnitStatus.AWAKE);
+                String message = UnitController.removeOneOrder(unit);
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setHeaderText("Success");
+                alert.setContentText(message.toString());
+                alert.show();
+            }
+        });
+
+        MenuItem awake = new MenuItem("awake");
+        awake.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if (unit.getStatus().equals(UnitStatus.SLEEP)) unit.setStatus(UnitStatus.AWAKE);
+            }
+        });
+
+        MenuItem delete = new MenuItem("delete");
+        delete.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                UnitController.deleteUnit(unit);
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setHeaderText("Success");
+                alert.setContentText("The unit is deleted successfully");
+                alert.show();
+                stage.close();
+            }
+        });
+
+        contextMenu.getItems().add(sleep);
+        contextMenu.getItems().add(ready);
+        contextMenu.getItems().add(improving);
+        contextMenu.getItems().add(improvingForHealth);
+        contextMenu.getItems().add(established);
+        contextMenu.getItems().add(rangedFight);
+        contextMenu.getItems().add(meleeFight);
+        contextMenu.getItems().add(readyRangedFight);
+        contextMenu.getItems().add(plundering);
+        contextMenu.getItems().add(deleteOrder);
+        contextMenu.getItems().add(awake);
+        contextMenu.getItems().add(delete);
+        return contextMenu;
+    }
+
     private void addToList() {
         for (Unit unit: player.getUnits()) {
             Label label = new Label("-unit type: " + unit.getMilitaryType().name() + " ground number: " +
@@ -58,7 +210,7 @@ public class UnitPanel extends Menus {
             label.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent mouseEvent) {
-
+                    buildContextMenu(unit).show(label, mouseEvent.getScreenX(), mouseEvent.getScreenY());
                 }
             });
             list.getChildren().add(label);
@@ -68,5 +220,9 @@ public class UnitPanel extends Menus {
             Label text = new Label("-Sorry, but you haven't have any units, yet");
             list.getChildren().add(text);
         }
+    }
+
+    public void back(MouseEvent mouseEvent) {
+        stage.close();
     }
 }
