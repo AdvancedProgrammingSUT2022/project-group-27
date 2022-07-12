dc7d75650afc6aca7745d6c786831d04f6b85cb9
Arefe Boushehrian <arefeboushehrian@Arefes-MacBook-Pro.local>
2022-07-12 01:21:29 -0700

    adding popup for unit information

--- src/main/java/model/UnitRectangle.java
+++ src/main/java/model/UnitRectangle.java
@@ -2,13 +2,21 @@ package model;
 
 
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import viewControllers.GraphicOfGame;
 
 public class UnitRectangle extends Circle {
     private Unit unit;
+    private static HBox hbox;
+    private static Unit unitSelect = null;
+    private static Stage stage = new Stage();
 
     public Unit getUnit() {
         return unit;
@@ -22,9 +30,26 @@ public class UnitRectangle extends Circle {
         this.setRadius(30);
        // this.setFill(Color.AQUA);
         this.setFill(unit.getMilitaryType().getImage());
+        hbox = new HBox();
+        unitSelect = null;
+        stage.close();
         this.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
+                Circle circle = new Circle();
+                circle.setFill(unit.getMilitaryType().getImage());
+                circle.setRadius(30);
+                Label label = new Label("unit name: " + unit.getMilitaryType().name() +
+                        " combat type: " + unit.getMilitaryType().getCombatType() +
+                        "\ncombat strength: " + unit.getMilitaryType().getCombatStrength() +
+                        " range and ranged strength: " + unit.getMilitaryType().getRange() + ", "
+                        + unit.getMilitaryType().getRangedCombatStrength() +
+                        "\nmp: " + unit.getMp() + " hp: " + unit.getHp() +
+                        " status: " + unit.getStatus());
+
+                hbox = new HBox(circle, label);
+                unitSelect = unit;
+
                 if (GlobalVariables.unitRectangle==null){
                     GlobalVariables.unitRectangle=UnitRectangle.this;
                     UnitRectangle.this.setStroke(Color.RED);
@@ -37,7 +62,16 @@ public class UnitRectangle extends Circle {
                     }
                     else{
                         GlobalVariables.unitRectangle=null;
+                        hbox = new HBox();
+                        unitSelect = null;
+                        stage.close();
                     }
+                }
+
+                Scene scene = new Scene(hbox);
+                stage.setScene(scene);
+                if (!stage.isShowing() && unitSelect != null) {
+                    stage.show();
                 }
             }
         });
